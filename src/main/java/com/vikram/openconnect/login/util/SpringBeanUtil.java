99b12b1b4897086a8ffd8ddc1c55f561e77a2ea9
Vikram Pyati <vipyati@ebay.com>
2015-04-18 18:04:01 +0530

    Adding ApplicationContexgt aware

--- src/main/java/com/vikram/openconnect/login/util/SpringBeanUtil.java
+++ src/main/java/com/vikram/openconnect/login/util/SpringBeanUtil.java
@@ -1,11 +1,11 @@
 package com.vikram.openconnect.login.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class SpringBeanUtil {
+public class SpringBeanUtil implements ApplicationContextAware {
 	
-	@Autowired
 	private ApplicationContext ctx;
 	
 	private static SpringBeanUtil instance = new SpringBeanUtil();
@@ -21,5 +21,10 @@ public class SpringBeanUtil {
 	
 	public <T> T getBean(String name, Class<T> claz){		
 		return ctx.getBean(name, claz);
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.ctx = applicationContext;
 	}
 }
