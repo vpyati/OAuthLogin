e92430822aecb61810771f00667443b2da0849b9
Vikram Pyati <vipyati@ebay.com>
2015-04-19 00:55:20 +0530

    Fixing error

--- src/main/java/com/vikram/openconnect/login/OlaAppConfig.java
+++ src/main/java/com/vikram/openconnect/login/OlaAppConfig.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.vikram.openconnect.login.identity.IdentityResovler;
 
 @Configuration
+@EnableWebMvc
 public class OlaAppConfig extends WebMvcConfigurerAdapter{
 
 	
